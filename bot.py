d84166078746b3b12f5e60abb77c0d278d937853
floozzy <flooozzy@gmail.com>
2026-07-22 16:05:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-import logging
+import os
+
 
 from telegram.ext import (
     Application,
@@ -8,73 +9,114 @@ from telegram.ext import (
     filters
 )
 
-from config import TOKEN
 
-from database.models import init_db
 
 from handlers.start import start_command
-from handlers.buttons import button_handler
+
 from handlers.profile import profile_command
-from handlers.files import file_handler
-from handlers.image_menu import image_menu
+
+from handlers.buttons import button_handler
+
+from handlers.text_input import text_input_handler
 
 
-logging.basicConfig(
-    level=logging.INFO
+
+TOKEN = os.getenv(
+    "BOT_TOKEN"
 )
+
+
+
+if not TOKEN:
+
+    TOKEN = "YOUR_BOT_TOKEN"
+
+
 
 
 def main():
 
-    init_db()
+
+    app = Application.builder().token(
+        TOKEN
+    ).build()
 
 
-    app = (
-        Application
-        .builder()
-        .token(TOKEN)
-        .build()
-    )
+
+    # ======================
+    # COMMANDS
+    # ======================
 
 
     app.add_handler(
+
         CommandHandler(
             "start",
             start_command
         )
+
     )
 
 
     app.add_handler(
+
         CommandHandler(
             "profile",
             profile_command
         )
+
     )
 
 
+
+    # ======================
+    # BUTTONS
+    # ======================
+
+
     app.add_handler(
+
         CallbackQueryHandler(
             button_handler
         )
+
     )
+
+
+
+    # ======================
+    # TEXT INPUT
+    # ======================
 
 
     app.add_handler(
+
         MessageHandler(
-            filters.Document.ALL,
-            file_handler
+
+            filters.TEXT
+            &
+            ~filters.COMMAND,
+
+            text_input_handler
+
         )
+
     )
+
 
 
     print(
-        "🚀 Convertly запущен!"
+        "🚀 Convertly запущен успешно!"
     )
+
 
 
     app.run_polling()
 
 
+
+
+
 if __name__ == "__main__":
+
     main()
