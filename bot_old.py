9d9c3ca0242b8e40bab5859535319323e71d78fb
floozzy <flooozzy@gmail.com>
2026-07-22 18:08:36 +0300

    Convertly v2

---
+++ bot_old.py
@@ -0,0 +1,47 @@
+ from telegram.ext import (
+        Application,
+            CommandHandler,
+                MessageHandler,
+                    filters
+                    )
+
+                    from config import TOKEN
+
+                    from handlers.start import start_command
+                    from handlers.files import file_handler
+
+
+                    def main():
+
+                        app = Application.builder().token(TOKEN).build()
+
+
+                            app.add_handler(
+                                        CommandHandler(
+                                                        "start",
+                                                                    start_command
+                                                                            )
+                                                                                )
+
+
+                                                                                    app.add_handler(
+                                                                                                MessageHandler(
+                                                                                                                filters.Document.ALL,
+                                                                                                                            file_handler
+                                                                                                                                    )
+                                                                                                                                        )
+
+
+                                                                                                                                            print("🚀 Convertly 3.0 запущен!")
+
+                                                                                                                                                app.run_polling()
+
+
+
+                                                                                                                                                if name == "main":
+                                                                                                                                                    main()
+                                                                                                )
+                                                                                    )
+                                        )
+                            )
+ )
