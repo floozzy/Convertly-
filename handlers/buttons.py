00412543fc743df6ad094ef6cf2ee9887ef29b50
floozzy <flooozzy@gmail.com>
2026-07-22 09:59:30 +0300

    Update buttons.py

--- handlers/buttons.py
+++ handlers/buttons.py
@@ -1,6 +1,8 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
+from handlers.convert import convert_menu
+
 
 async def button_handler(
     update: Update,
@@ -14,28 +16,27 @@ async def button_handler(
 
     if query.data == "convert":
 
-        await query.edit_message_text(
-            "📂 Отправьте мне файл.\n\n"
-            "Я определю его формат и предложу варианты конвертации."
+        await convert_menu(
+            update,
+            context
         )
 
 
     elif query.data == "profile":
 
         await query.edit_message_text(
-            "👤 Ваш профиль:\n\n"
+            "👤 Профиль\n\n"
             "Файлов обработано: 0\n"
-            "Статус: Бесплатный"
+            "Статус: Free"
         )
 
 
     elif query.data == "help":
 
         await query.edit_message_text(
-            "ℹ️ Convertly умеет работать с файлами.\n\n"
-            "Скоро появятся:\n"
+            "ℹ️ Convertly 3.1\n\n"
+            "Поддерживаемые функции:\n"
             "🖼 Изображения\n"
-            "📄 PDF\n"
-            "🎵 Аудио\n"
-            "🎬 Видео"
+            "📄 PDF (скоро)\n"
+            "🎵 Аудио (скоро)"
         )
