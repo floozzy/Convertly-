b523050ceb08ab75bf858c1536d5656014c84800
floozzy <flooozzy@gmail.com>
2026-07-22 12:35:51 +0300

    Update profile.py

--- touch handlers/profile.py
+++ touch handlers/profile.py
@@ -1,1 +1,66 @@
+from telegram import Update
+from telegram.ext import ContextTypes
 
+from services.user_service import UserService
+
+
+async def profile_command(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
+
+    user_id = update.effective_user.id
+
+    profile = UserService.profile(
+        user_id
+    )
+
+    if profile is None:
+
+        await update.message.reply_text(
+            "❌ Профиль не найден.\n"
+            "Нажмите /start"
+        )
+
+        return
+
+
+    status = (
+        "👑 Premium"
+        if profile["premium"]
+        else
+        "🆓 Free"
+    )
+
+
+    username = (
+        f"@{profile['username']}"
+        if profile["username"]
+        else
+        "нет"
+    )
+
+
+    text = (
+        "👤 <b>Ваш профиль Convertly</b>\n\n"
+
+        f"🆔 ID: <code>{profile['telegram_id']}</code>\n"
+        f"🙍 Имя: {profile['first_name']}\n"
+        f"📛 Username: {username}\n\n"
+
+        f"📌 Статус: {status}\n"
+        f"⭐ Stars: {profile['stars']}\n\n"
+
+        f"📂 Файлов обработано: {profile['files']}\n"
+        f"📜 Операций: {profile['operations']}\n"
+        f"💸 Потрачено Stars: {profile['spent']}\n\n"
+
+        f"📅 Регистрация:\n"
+        f"{profile['created']}"
+    )
+
+
+    await update.message.reply_text(
+        text,
+        parse_mode="HTML"
+    )
