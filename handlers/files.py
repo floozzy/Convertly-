aa51483a7ca3d298cbde6d8c0604b931cd02d34d
floozzy <flooozzy@gmail.com>
2026-07-22 09:50:04 +0300

    Refactor file_handler for better readability
    Refactor file_handler to improve readability and structure. Added checks for document presence and updated response messages.

--- handlers/files.py
+++ handlers/files.py
@@ -1,15 +1,59 @@
+import os
+
 from telegram import Update
 from telegram.ext import ContextTypes
 
+from services.image_converter import convert_to_png
 
-async def file_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-    if update.message.document:
+async def file_handler(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
 
-        filename = update.message.document.file_name
+    document = update.message.document
+
+    if not document:
+        return
+
+
+    filename = document.file_name
+
+    await update.message.reply_text(
+        "📥 Получил файл!\n"
+        f"📄 {filename}\n\n"
+        "⚙️ Начинаю обработку..."
+    )
+
+
+    file = await document.get_file()
+
+
+    input_path = f"files/{filename}"
+
+
+    await file.download_to_drive(
+        input_path
+    )
+
+
+    if filename.lower().endswith(
+        (".jpg", ".jpeg", ".webp")
+    ):
+
+        output = convert_to_png(
+            input_path
+        )
+
+
+        await update.message.reply_document(
+            document=open(output, "rb"),
+            caption="✅ Готово! Конвертация JPG → PNG"
+        )
+
+
+    else:
 
         await update.message.reply_text(
-            "📥 Файл получен!\n\n"
-            f"📄 {filename}\n\n"
-            "⚙️ Скоро начну обработку."
+            "❌ Пока этот формат не поддерживается."
         )
