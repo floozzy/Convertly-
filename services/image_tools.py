c9cc93a26d2228efa848772212bc7885671ef675
floozzy <flooozzy@gmail.com>
2026-07-22 12:07:59 +0300

    Update image_tools.py

--- services/image_tools.py
+++ services/image_tools.py
@@ -2,84 +2,36 @@ from PIL import Image
 import os
 
 
-def convert_image(
-    input_path,
-    output_format
-):
 
-    image = Image.open(input_path)
+def rotate_image(path, angle):
 
-    output_path = (
-        input_path.rsplit(".", 1)[0]
-        + "."
-        + output_format.lower()
+    image = Image.open(path)
+
+    image = image.rotate(
+        angle,
+        expand=True
     )
 
 
-    if output_format.lower() == "jpg":
-        image = image.convert("RGB")
-
-
-    image.save(
-        output_path,
-        output_format.upper()
-    )
-
-    return output_path
-
-
-
-def compress_image(
-    input_path,
-    quality=60
-):
-
-    image = Image.open(input_path)
-
-    output_path = (
-        input_path.rsplit(".", 1)[0]
-        + "_compressed.jpg"
+    output = (
+        path.rsplit(".",1)[0]
+        + "_rotated.png"
     )
 
 
-    image.convert(
-        "RGB"
-    ).save(
-        output_path,
-        "JPEG",
-        quality=quality
-    )
+    image.save(output)
 
-
-    return output_path
+    return output
 
 
 
-def resize_image(
-    input_path,
-    width,
-    height
-):
 
-    image = Image.open(input_path)
+def image_info(path):
 
-    image = image.resize(
-        (
-            width,
-            height
-        )
-    )
+    image = Image.open(path)
 
-
-    output_path = (
-        input_path.rsplit(".",1)[0]
-        + "_resized.png"
-    )
-
-
-    image.save(
-        output_path
-    )
-
-
-    return output_path
+    return {
+        "width": image.width,
+        "height": image.height,
+        "format": image.format
+    }
