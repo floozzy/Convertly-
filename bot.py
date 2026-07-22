a77fa53b2d8a299e6a2bac1a7fec841d8107a60f
floozzy <flooozzy@gmail.com>
2026-07-22 10:01:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ from config import TOKEN
 from handlers.start import start_command
 from handlers.files import file_handler
 from handlers.buttons import button_handler
+from handlers.convert import convert_menu
 
 
 def main():
