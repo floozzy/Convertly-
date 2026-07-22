3c55323d0db39a0835ad6d76ea18fb88019faba3
floozzy <flooozzy@gmail.com>
2026-07-22 16:01:28 +0300

    Update state.py

--- utils/state.py
+++ utils/state.py
@@ -1,9 +1,48 @@
-user_files = {}
+user_states = {}
 
 
-def save_file(user_id, path):
-    user_files[user_id] = path
+
+def set_state(
+    user_id,
+    key,
+    value
+):
+
+    if user_id not in user_states:
+
+        user_states[user_id] = {}
 
 
-def get_file(user_id):
-    return user_files.get(user_id)
+    user_states[user_id][key] = value
+
+
+
+
+
+def get_state(
+    user_id,
+    key,
+    default=None
+):
+
+    if user_id not in user_states:
+
+        return default
+
+
+    return user_states[user_id].get(
+        key,
+        default
+    )
+
+
+
+
+
+def clear_state(
+    user_id
+):
+
+    if user_id in user_states:
+
+        del user_states[user_id]
